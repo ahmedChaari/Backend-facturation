a80f0b8ace9c4362701d9e2e208de38f363f2724
AhmedChaari <ahmed17ch@gmail.com>
2022-10-10 00:45:01 +0100

    root for login and logout

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,26 @@
 <?php
 
+use App\Http\Controllers\API\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+/*************************************
+ *          Public routes            *
+ *                                   *
+ *************************************/
+
+Route::post('/login',               [AuthController::class,  'login'])->name('login.admin');
+
+
+/*************************************
+ *      Protected routes Auth        *
+ *                                   *
+ *************************************/
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::post('logout',             [AuthController::class, 'logout']);
 });
