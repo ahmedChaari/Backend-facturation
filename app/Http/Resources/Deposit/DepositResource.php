7eea14b0416673175c3cc6f0bd5e82a48ff9a047
AhmedChaari <ahmed17ch@gmail.com>
2023-01-04 12:52:34 +0100

    get id

--- app/Http/Resources/Deposit/DepositResource.php
+++ app/Http/Resources/Deposit/DepositResource.php
@@ -15,6 +15,7 @@ class DepositResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'company'=> $this->company->name,
            // 'created_at' =>$this->created_at,
